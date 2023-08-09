5c21401506ba14be60436eca10d1ea515f224e75
unknown <jakub.zezula@hotmail.com>
2023-08-09 23:43:10 +0200

    Update

--- MagicEightBallServiceHost/MagicEightBallServiceHost/Program.cs
+++ MagicEightBallServiceHost/MagicEightBallServiceHost/Program.cs
@@ -17,10 +17,27 @@ namespace MagicEightBallServiceHost
             using (ServiceHost serviceHost = new ServiceHost(typeof(MagicEightBallService)))
             {
                 serviceHost.Open();
+                DisplayHostInfo(serviceHost);
 
                 Console.WriteLine("The service is ready.");
                 Console.WriteLine("Press the Enter key to terminate service.");
                 Console.ReadLine();
+            }
+        }
+
+        static void DisplayHostInfo(ServiceHost host)
+        {
+            Console.WriteLine();
+            Console.WriteLine("***** Host Info *****");
+
+            foreach(System.ServiceModel.Description.ServiceEndpoint se in host.Description.Endpoints)
+            {
+                Console.WriteLine("Adress: {0}", se.Address);
+                Console.WriteLine("Binding: {0}", se.Binding.Name);
+                Console.WriteLine("Contract: {0}", se.Contract.Name);
+                Console.WriteLine();
+
+                Console.WriteLine("**********************************************");
             }
         }
     }
